ba4ebacfc2097a5da3c6f77b6c458bdcd119d16b
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-12 17:48:15 +0300

    При клике на стрелочку открывается второй номер телефона

--- src/modules/showPhone .js
+++ src/modules/showPhone .js
@@ -1,6 +1,24 @@
 const showPhone = () => {
-    const showPhone = 'showPhone';
-    console.log(showPhone);
+    const headerContacts = document.querySelector('.header-contacts__phone-number-accord');
+    const headerPhoneNumber = document.querySelectorAll('.header-contacts__phone-number')[1];
+    const headerContactsArrow = document.querySelector('.header-contacts__arrow');
+
+    console.log(headerPhoneNumber);
+    
+    headerContactsArrow.addEventListener('click', () => {
+        if (headerPhoneNumber.style.opacity == '0') {
+            headerContacts.style.top = '25px';
+            headerPhoneNumber.style.opacity = '1';
+            headerContactsArrow.style.transform = 'rotate(-180deg)';
+        } else {
+            headerContacts.style.top = '0';
+            headerPhoneNumber.style.opacity = '0';
+            headerContactsArrow.style.transform = 'rotate(0deg)';
+        }
+
+    });
+
+    
 };
 
 export default showPhone;
