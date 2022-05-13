f72d80bd0f1543c14ebe7bf233c171b25d871755
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-13 20:57:50 +0300

    При клике на надпись “Политика конфиденциальности”

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import showPhone from './modules/showPhone';
 import burgerMenu from './modules/burgerMenu';
+import showModalWindow from './modules/showModalWindow';
 
 showPhone();
 burgerMenu();
+showModalWindow();
---
+++ src/modules/showModalWindow.js
@@ -0,0 +1,21 @@
+const showModalWindow = () => {
+    const popupPrivacy = document.querySelector('.popup-privacy');
+    const closePrivacy = document.querySelector('.popup-privacy > .close');
+    const body = document.body;
+
+    console.log(popupPrivacy);
+    
+
+    body.addEventListener('click', (e) => {
+        if (e.target.closest('.link-privacy')) {
+            popupPrivacy.style.visibility = 'visible';
+        }
+        if (e.target.closest('.popup-privacy > .close')) {
+            popupPrivacy.style.visibility = 'hidden';
+        }
+
+    });
+
+};
+
+export default showModalWindow;
