20a4ee940e12c91985eee25538f9cfebe58d3312
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-17 16:48:19 +0300

    Готов функционал модального окна (Консультация)

--- src/modules/questionsAccordion.js
+++ src/modules/questionsAccordion.js
@@ -2,7 +2,6 @@ const questionsAccordion = () => {
     const accordion = document.querySelector('.accordion');
     const accordionList = document.querySelectorAll('.accordion > ul > li > h2');
 
-    console.log(accordionList);
     accordion.addEventListener('click', (e) => {
         if (e.target.closest('.accordion > ul > li > h2')) {
             accordionList.forEach((elem) => {
@@ -12,7 +11,6 @@ const questionsAccordion = () => {
                     setTimeout(() => {
                         
                         while (elem.getBoundingClientRect().top < 10) {
-                            console.log(elem.getBoundingClientRect().top);
                             document.documentElement.scrollTop = document.documentElement.scrollTop - 50;
                         }
                     }, 800);
--- src/modules/showModalWindow.js
+++ src/modules/showModalWindow.js
@@ -1,14 +1,24 @@
 const showModalWindow = () => {
     const popupPrivacy = document.querySelector('.popup-privacy');
     const body = document.body; 
+    const consultations = document.querySelector('.popup-consultation');
 
     body.addEventListener('click', (e) => {
+        // Политика конфиденциальности
         if (e.target.closest('.link-privacy')) {
             popupPrivacy.style.visibility = 'visible';
         }
         if (e.target.closest('.popup-privacy > .close') || e.target.closest('.popup-dialog > .close')) {
             popupPrivacy.style.visibility = 'hidden';
         }
+        // консультация
+        if (e.target.closest('button.button_wide')) {
+            consultations.style.visibility = 'visible';
+        }
+        if (e.target.closest('.close-consultation')) {
+            consultations.style.visibility = '';
+        }
+
 
     });
 };
