07030c5f90feb718cfe1fa3ab19173b22bd18d83
Anton Anokhin <anoxin.toscha1@yandex.ru>
2022-05-20 02:07:57 +0300

    реализована фильтрация по услугам

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,19 +1,19 @@
-// import { render } from "./modules/render";
+import { render } from "./modules/render";
 import { renderLogin } from "./modules/renderLogin";
 // import { addUsers } from "./modules/addUsers";
-// import { UserService } from "./modules/userService";
+import { UserService } from "./modules/userService";
 // import { removeUsers } from "./modules/removeUsers";
 // import { changePermissions } from "./modules/changePermissions";
 // import { editUsers } from "./modules/editUsers";
 import { LoginService } from "./modules/loginService";
 import { loginTable } from "./modules/loginTable";
 
-// window.userService = new UserService;
+window.userService = new UserService;
 window.loginService = new LoginService;
 
-// userService.getUsers().then(data => {
-//     render(data);
-// });
+userService.getUsers().then(data => {
+    render(data);
+});
 loginService.getUsers().then(data => {
     renderLogin(data);
 });
--- src/backend/modules/render.js
+++ src/backend/modules/render.js
@@ -1,33 +1,60 @@
 export const render = (users) => {
-    const tboby = document.getElementById('table-body');
-
+    const tboby = document.getElementById('table');
+    const select = document.getElementById('typeItem');
+    let arrType = [];
     tboby.innerHTML = '';
+    select.innerHTML = '';
 
     users.forEach(user => {
         tboby.insertAdjacentHTML('beforeend', `
-            <tr data-key="${user.id}">
-                <th scope="row">${user.id}</th>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.children ? 'Есть' : 'Нет'}</td>
-                <td>
-                    <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch"
-                            id="form-children" ${user.permissions ? 'checked' : ''}>
-                    </div>
+            <tr class="table__row">
+                <td class="table__id table__cell">${user.id}</td>
+                <td class="table-type table__cell">${user.type}</td>
+                <td class="table-name table__cell">
+                    ${user.name}
+                </td>
+                <td class="table-units table__cell">
+                    ${user.units}
+                </td>
+                <td class="table-cost table__cell">
+                    ${user.cost} руб
                 </td>
                 <td>
-                    <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                        <button type="button" class="btn btn-warning btn-edit">
-                            <i class="bi-pencil-square"></i>
+                    <div class="table__actions table__cell">
+                        <button class="button action-change"><span class="svg_ui"><svg class="action-icon_change"><use xlink:href="./img/sprite.svg#change"></use></svg></span><span>Изменить</span>
                         </button>
-                        <button type="button" class="btn btn-danger btn-remove">
-                            <i class="bi-person-x"></i>
+                        <button class="button action-remove"><span class="svg_ui"><svg class="action-icon_remove"><use xlink:href="./img/sprite.svg#remove"></use></svg></span><span>Удалить</span>
                         </button>
                     </div>
                 </td>
             </tr>
             
         `);
+        arrType.push(user.type);
+
+    });
+
+    let newArr = Array.from(new Set(arrType));
+
+    select.insertAdjacentHTML('beforeend', `
+        <option value="Все услуги">Все услуги</option>  
+    `);
+    
+    newArr.forEach(elem => {
+        select.insertAdjacentHTML('beforeend', `
+            <option value="${elem}">${elem}</option>  
+        `);
+
+    });
+
+    select.addEventListener('change', () => {
+        tboby.querySelectorAll('.table__row').forEach(elem => {
+            if (elem.innerHTML.search(`${select.value}`) == -1 && select.value != "Все услуги") {
+                elem.style.display = "none";
+            } else {
+                elem.style.display = "";
+            }
+        });
+
     });
 };
